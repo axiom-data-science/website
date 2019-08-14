491adc80a27eb9ed080afd7d71146f23ca31140f
Kyle Wilcox <kyle@axiomdatascience.com>
2019-08-14 19:10:25 -0400

    Adding home and client

--- src/templates/ClientPage.js
+++ src/templates/ClientPage.js
@@ -1,3 +1,91 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
-export default () => <div>Client Page (dynamic)!</div>
+import Layout from "../components/Layout"
+
+export const ClientPageTemplate = ({
+  title,
+  intro,
+  featuredImage,
+  body,
+  clients,
+}) => (
+  <main className="Contact">
+    <p>{title}</p>
+
+    <p>{intro}</p>
+
+    <div>
+      <Img fluid={featuredImage.childImageSharp.fluid} />
+    </div>
+
+    {clients.nodes.map(client => {
+      let c = client.frontmatter
+      return (
+        <section>
+          <div>
+            <a href={c.url} target="_blank" rel="noopener noreferrer">
+              <Img fixed={c.logo.childImageSharp.fixed} />
+            </a>
+          </div>
+        </section>
+      )
+    })}
+  </main>
+)
+
+const ClientPage = ({ data: { page, clients } }) => (
+  <Layout
+    meta={page.frontmatter.meta || false}
+    title={page.frontmatter.title || false}
+  >
+    <ClientPageTemplate
+      {...page.frontmatter}
+      clients={clients}
+      body={page.html}
+    />
+  </Layout>
+)
+
+export default ClientPage
+
+export const pageQuery = graphql`
+  query ClientPage($id: String!) {
+    page: markdownRemark(id: { eq: $id }) {
+      ...Meta
+      html
+      frontmatter {
+        template
+        title
+        intro
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 1500) {
+              ...GatsbyImageSharpFluid_noBase64
+            }
+          }
+        }
+      }
+    }
+
+    clients: allMarkdownRemark(
+      filter: { fields: { contentType: { eq: "clients" } } }
+    ) {
+      nodes {
+        frontmatter {
+          name
+          slug
+          url
+          logo {
+            childImageSharp {
+              fixed(width: 200) {
+                ...GatsbyImageSharpFixed_noBase64
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -1,3 +1,57 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
-export default () => <div>Home Page!</div>
+import Layout from "../components/Layout"
+
+export const HomePageTemplate = ({ title, intro, images, body }) => (
+  <main className="Contact">
+    <p>{title}</p>
+
+    <p>{intro}</p>
+
+    {images.map(img => {
+      return (
+        <section>
+          <div>
+            <Img fluid={img.image.childImageSharp.fluid} />
+          </div>
+        </section>
+      )
+    })}
+  </main>
+)
+
+const HomePage = ({ data: { page } }) => (
+  <Layout
+    meta={page.frontmatter.meta || false}
+    title={page.frontmatter.title || false}
+  >
+    <HomePageTemplate {...page.frontmatter} body={page.html} />
+  </Layout>
+)
+
+export default HomePage
+
+export const pageQuery = graphql`
+  query HomePage($id: String!) {
+    page: markdownRemark(id: { eq: $id }) {
+      ...Meta
+      html
+      frontmatter {
+        template
+        title
+        intro
+        images {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 1500) {
+                ...GatsbyImageSharpFluid_noBase64
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
