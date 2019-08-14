3c011aecfff0b4afca0ba88008a45d5ab045c998
Kyle Wilcox <kyle@axiomdatascience.com>
2019-08-14 10:03:15 -0400

    Updating gatsby plugin setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,66 @@ module.exports = {
 
   siteMetadata: {
     title: 'Axiom Data Science',
-    siteUrl: 'https://axiomdatascience.com'
+    siteUrl: 'https://axiom-website.netlify.com/'
   },
 
-  plugins: [`gatsby-plugin-netlify-cms`]
+  plugins: [
+    'gatsby-plugin-react-helmet',
 
+    'gatsby-transformer-yaml',
+
+    // Convert files (JSON, Markdown) to React objects we can use in components
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: 'assets'
+      }
+    },
+
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content`,
+        name: 'pages'
+      }
+    },
+
+    // Images
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-relative-images`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800
+            },
+          },
+        ],
+      },
+    },
+
+    'gatsby-plugin-sass',
+
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        // Setting a color is optional.
+        color: 'white',
+        // Disable the loading spinner.
+        showSpinner: false
+      }
+    },
+
+    'gatsby-plugin-sitemap',
+
+    'gatsby-plugin-netlify-cms',
+
+    'gatsby-plugin-netlify'
+
+  ]
 }
