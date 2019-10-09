94fda4b3087a42065dc5ad362c5af11cf69cb362
Dennis Glasberg <dennis.glasberg@flywire.com>
2019-10-09 19:42:28 -0400

    Created CSS Reset, ThemeProvider and GlobalStyle for styled-components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react"
 import Helmet from "react-helmet"
-import Header from './Header';
-import Footer from './Footer';
+import Header from "./Header"
+import Footer from "./Footer"
 import { StaticQuery, graphql } from "gatsby"
+import styled, { ThemeProvider } from "styled-components"
 
+import { Theme, GlobalStyle } from "./Theme"
 import Meta from "./Meta"
-
-import "modern-normalize/modern-normalize.css"
 
 export default ({ children, meta, title }) => {
   return (
@@ -28,29 +28,31 @@ export default ({ children, meta, title }) => {
           data.settingsYaml || {}
 
         return (
-          <Fragment>
-            <Helmet
-              defaultTitle={siteTitle}
-              titleTemplate={`%s | ${siteTitle}`}
-            >
-              {title}
-              {/* Add font link tags here */}
-            </Helmet>
-
-            <Meta
-              googleTrackingId={googleTrackingId}
-              absoluteImageUrl={
-                socialMediaCard &&
-                socialMediaCard.image &&
-                socialMediaCard.image
-              }
-              {...meta}
-              {...data.settingsYaml}
-            />
-            <Header></Header>
-            <Fragment>{children}</Fragment>
-            <Footer></Footer>
-          </Fragment>
+          <ThemeProvider theme={Theme}>
+            <Fragment>
+              <Helmet
+                defaultTitle={siteTitle}
+                titleTemplate={`%s | ${siteTitle}`}
+              >
+                {title}
+                {/* Add font link tags here */}
+              </Helmet>
+              <Meta
+                googleTrackingId={googleTrackingId}
+                absoluteImageUrl={
+                  socialMediaCard &&
+                  socialMediaCard.image &&
+                  socialMediaCard.image
+                }
+                {...meta}
+                {...data.settingsYaml}
+              />
+              <GlobalStyle />
+              <Header />
+              <Fragment>{children}</Fragment>
+              <Footer />
+            </Fragment>
+          </ThemeProvider>
         )
       }}
     />
