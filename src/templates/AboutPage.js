1091d027da6d6ad96191997f0c90bd42946c2c1b
Dennis Glasberg <dennis.glasberg@flywire.com>
2019-10-18 00:52:03 -0400

    Updated Staff List Style

--- src/templates/AboutPage.js
+++ src/templates/AboutPage.js
@@ -14,7 +14,7 @@ const StyledSectionTitle = styled.h4`
   color: black;
 `
 
-const StyledLocations = styled.div`
+const StyledLocationList = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
   grid-gap: 20px;
@@ -40,68 +40,101 @@ const StyledAddress = styled.div`
   display: flex;
   flex-direction: column;
 `
+const StyledStaffList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 20px;
+  max-width: ${props => props.theme.maxWidth};
+  a {
+    text-decoration: none;
+    color: black;
+  }
+`
+
+const StyledStaff = styled.div`
+  display: grid;
+  grid-template-columns: 200px 1fr;
+  grid-gap: 20px;
+  max-width: ${props => props.theme.maxWidth};
+  padding: 1rem;
+  border-radius: 4px;
+  border: 1px solid black;
+`
+
+const StyledStaffInformation = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  h3 {
+    margin-top: 0;
+  }
+`
 
 export const AboutPageTemplate = ({
-         title,
-         subtitle,
-         featured,
-         body,
-         locations,
-         staff,
-       }) => (
-         <main>
-           <PageMeta title={title} subtitle={subtitle} featured={featured} />
-           <StyledSectionTitle>Locations</StyledSectionTitle>
-           <StyledLocations>
-             {locations.map(loc => {
-               let l = loc.frontmatter
-               return (
-                 <section>
-                   <div>
-                     <div>
-                       <div>
-                         {l.address1 && l.address2 && (
-                           <a
-                             href={l.mapslink}
-                             target="_blank"
-                             rel="noopener noreferrer"
-                           >
-                             <Img fluid={l.photo.childImageSharp.fluid} />
-                             <StyledGeo>
-                               <MapPin />
-                               <StyledAddress>
-                                 <p>{l.address1}</p>
-                                 <p>{l.address2}</p>
-                               </StyledAddress>
-                             </StyledGeo>
-                           </a>
-                         )}
-                       </div>
-                     </div>
-                   </div>
-                 </section>
-               )
-             })}
-           </StyledLocations>
-           <StyledSectionTitle>Staff</StyledSectionTitle>
-           {staff.map(s => {
-             let l = s.frontmatter
-             return (
-               <section>
-                 <div>
-                   <Img fixed={l.headshot.childImageSharp.fixed} />
-                   {l.name}
-                   {l.position}
-                   {l.email}
-                   <Content source={s.html} />
-                 </div>
-               </section>
-             )
-           })}
-           <h1>Content</h1>
-           <Content source={body} />
-         </main>
-       )
+  title,
+  subtitle,
+  featured,
+  body,
+  locations,
+  staff,
+}) => (
+  <main>
+    <PageMeta title={title} subtitle={subtitle} featured={featured} />
+    <StyledSectionTitle>Locations</StyledSectionTitle>
+    <StyledLocationList>
+      {locations.map(loc => {
+        let l = loc.frontmatter
+        return (
+          <section>
+            <div>
+              <div>
+                <div>
+                  {l.address1 && l.address2 && (
+                    <a
+                      href={l.mapslink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Img fluid={l.photo.childImageSharp.fluid} />
+                      <StyledGeo>
+                        <MapPin />
+                        <StyledAddress>
+                          <p>{l.address1}</p>
+                          <p>{l.address2}</p>
+                        </StyledAddress>
+                      </StyledGeo>
+                    </a>
+                  )}
+                </div>
+              </div>
+            </div>
+          </section>
+        )
+      })}
+    </StyledLocationList>
+    <StyledSectionTitle>Staff</StyledSectionTitle>
+    <StyledStaffList>
+      {staff.map(s => {
+        let l = s.frontmatter
+        return (
+          <StyledStaff>
+            <Img fluid={l.headshot.childImageSharp.fluid} />
+            <StyledStaffInformation>
+              <a href={`mailto:${l.email}`}>
+                <h3>{l.name}</h3>
+              </a>
+              <p>{l.position}</p>
+              {l.email}
+              <Content source={s.html} />
+            </StyledStaffInformation>
+          </StyledStaff>
+        )
+      })}
+    </StyledStaffList>
+    <h1>Content</h1>
+    <Content source={body} />
+  </main>
+)
 
 const AboutPage = ({ data: { page, locations, staff } }) => (
   <Layout
@@ -120,66 +153,66 @@ const AboutPage = ({ data: { page, locations, staff } }) => (
 export default AboutPage
 
 export const pageQuery = graphql`
-         query AboutPage($id: String!) {
-           page: markdownRemark(id: { eq: $id }) {
-             ...Meta
-             html
-             frontmatter {
-               template
-               title
-               subtitle
-               featured {
-                 image {
-                   childImageSharp {
-                     fluid(maxWidth: 1500) {
-                       ...GatsbyImageSharpFluid_noBase64
-                     }
-                   }
-                 }
-               }
-             }
-           }
+  query AboutPage($id: String!) {
+    page: markdownRemark(id: { eq: $id }) {
+      ...Meta
+      html
+      frontmatter {
+        template
+        title
+        subtitle
+        featured {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 1500) {
+                ...GatsbyImageSharpFluid_noBase64
+              }
+            }
+          }
+        }
+      }
+    }
 
-           locations: allMarkdownRemark(
-             filter: { fields: { contentType: { eq: "locations" } } }
-           ) {
-             nodes {
-               frontmatter {
-                 address1
-                 address2
-                 title
-                 latitude
-                 longitude
-                 mapslink
-                 photo {
-                   childImageSharp {
-                     fluid(maxWidth: 500) {
-                       ...GatsbyImageSharpFluid_noBase64
-                     }
-                   }
-                 }
-               }
-             }
-           }
+    locations: allMarkdownRemark(
+      filter: { fields: { contentType: { eq: "locations" } } }
+    ) {
+      nodes {
+        frontmatter {
+          address1
+          address2
+          title
+          latitude
+          longitude
+          mapslink
+          photo {
+            childImageSharp {
+              fluid(maxWidth: 500) {
+                ...GatsbyImageSharpFluid_noBase64
+              }
+            }
+          }
+        }
+      }
+    }
 
-           staff: allMarkdownRemark(
-             filter: { fields: { contentType: { eq: "staff" } } }
-           ) {
-             nodes {
-               html
-               frontmatter {
-                 name
-                 email
-                 position
-                 headshot {
-                   childImageSharp {
-                     fixed(width: 200) {
-                       ...GatsbyImageSharpFixed_noBase64
-                     }
-                   }
-                 }
-               }
-             }
-           }
-         }
-       `
+    staff: allMarkdownRemark(
+      filter: { fields: { contentType: { eq: "staff" } } }
+    ) {
+      nodes {
+        html
+        frontmatter {
+          name
+          email
+          position
+          headshot {
+            childImageSharp {
+              fluid(maxWidth: 500) {
+                ...GatsbyImageSharpFluid_noBase64
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
